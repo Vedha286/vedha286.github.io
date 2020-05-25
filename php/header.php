7071c9debe36d19bfa67f5994320c4766d4d8382
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-05-25 20:16:29 +0200

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light" style="box-shadow: 0 0 10px 0 rgba(0,0,0,.1);">
+		<div class="container-fluid">
+			<a  class="navbar-brand" href="#">
+				<img src="../images/logo.png" alt="logo" />
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+				<span class="navbar-toggler-icon"> </span>
+			</button>
+			<div class="collapse navbar-collapse " id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link"style="color:rgba(0,0,0,.5); " href="index.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="color:rgba(0,0,0,.5); " href="aboutUs.php">About Us</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link"style="color:rgba(0,0,0,.5); " href="getInvolved.php">Get Involved</a>
+					</li>
+					<!--<li class="nav-item">
+						<a class="nav-link" style="color:rgba(0,0,0,.5); " href="stories.php">Stories</a>
+					</li>-->
+					<li class="nav-item">
+						<a class="nav-link"style="color:rgba(0,0,0,.5); " href="events.php">Events</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="color:rgba(0,0,0,.5); "href="gallery.php">Gallery</a>
+					</li>
+					<li class="nav-item">
+						&nbsp;
+					</li>
+					<li class="nav-item">
+					</li>
+				<ul/>
+			</div>
+		</div>
+	</nav>
